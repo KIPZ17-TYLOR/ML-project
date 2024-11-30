13f2838486f58f6956b4145f005fdc860638da56
KIPZ17-TYLOR <160477039+KIPZ17-TYLOR@users.noreply.github.com>
2024-11-30 19:54:21 +0300

    Add files via upload

---
+++ colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const baseGreen = Colors.green;
+final primaryGreen = Colors.green[400];
+final secondaryGreen = Colors.green[200];
+
+const baseDark = Colors.black;
+const primaryDark = Colors.black12;
+const secondaryDark = Colors.black26;
